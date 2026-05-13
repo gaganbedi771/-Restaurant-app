94f2a25905b58bd3304b9aef1952cb5686fdb884
gaganbedi771 <gaganbedi771@gmail.com>
2026-05-13 12:47:49 +0530

    summary

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Layout/Header";
+import MealsSummary from "./components/Meals/MealsSummary";
 
 
 function App() {
   return (
     <>
       <Header></Header>
+      <MealsSummary></MealsSummary>
     </>
   );
 }
